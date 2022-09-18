dc16577e9da602757e9531e56fee319a588e5cae
sheway <67420772+sheway@users.noreply.github.com>
2022-09-18 16:34:32 +0800

    Add files via upload

---
+++ FedAvg_server.py
@@ -0,0 +1,48 @@
+from typing import Callable
+from typing import Dict
+import flwr as fl
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from flwr.common import weights_to_parameters
+import numpy as np
+import os
+
+# os.environ['GRPC_TRACE'] = 'all'
+# os.environ['GRPC_VERBOSITY'] = 'DEBUG'
+
+
+#Configuring client fit and client evaluate
+def get_on_fit_config_fn() -> Callable[[int], Dict[str, str]]:
+    def fit_config(rnd: int) -> Dict[str, str]:
+        config = {
+            "learning_rate": str(0.001),
+            "batch_size": 32,
+        }
+        return config
+    return fit_config
+
+
+if __name__ == "__main__":
+    # model = Sequential()
+    # model.add(Conv2D(filters=32, kernel_size=3, input_shape=(1, 28, 28), activation='relu', padding='same'))
+    # model.add(MaxPool2D(pool_size=2, data_format='channels_first'))
+    # model.add(Flatten())
+    # model.add(Dense(256, activation='relu'))
+    # model.add(Dense(10, activation='softmax'))
+    # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.1,
+        min_available_clients=2,  #最少要幾個 client 才能執行
+        # initial_parameters=weights_to_parameters(model.get_weights()),
+       on_fit_config_fn=get_on_fit_config_fn(),
+    )
+
+    # Start Flower server
+    fl.server.start_server(
+        server_address="localhost:7001",
+        config={"num_rounds": 3},
+        strategy=strategy,
+        # grpc_max_message_length=1024 * 1024 * 1024,
+        force_final_distributed_eval=True
+    )
---
+++ client2.py
@@ -0,0 +1,78 @@
+import os
+import flwr as fl
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from keras.utils import np_utils
+import seaborn as sns
+
+# Make TensorFlow log less verbose
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+# os.environ['GRPC_TRACE'] = 'all'
+# os.environ['GRPC_VERBOSITY'] = 'DEBUG'
+
+
+def getDist(y):
+    ax = sns.countplot(y)
+    ax.set(title="Count of Client2 data classes")
+    plt.show()
+
+
+def getData(dist, x, y):
+    # print(x[1], len(x))
+    dx = []
+    dy = []
+    counts = [0 for i in range(10)]
+    for i in range(len(x)):
+        if counts[y[i]] < dist[y[i]]:
+            dx.append(x[i])
+            dy.append(y[i])
+            counts[y[i]] += 1
+    return np.array(dx), np.array(dy)
+
+
+# Load model and data
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=3, input_shape=(1, 28, 28), activation='relu', padding='same'))
+model.add(MaxPool2D(pool_size=2, data_format='channels_first'))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(60000, 1, 28, 28)/255
+x_test = x_test.reshape(10000, 1, 28, 28)/255
+
+dist = [0, 10, 10, 10, 4000, 3000, 4000, 5000, 10, 4500]
+x_train, y_train = getData(dist, x_train, y_train)
+getDist(y_train)
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+# Define Flower client
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        r = model.fit(x_train, y_train, epochs=1, batch_size=32)
+        # history = r.history
+        # print('Fit history:', history)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        # print('Eval accuracy:', accuracy)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+
+# Start Flower client
+fl.client.start_numpy_client(
+    "localhost:7001",
+    client=CifarClient(),
+    # grpc_max_message_length=1024 * 1024 * 1024
+)
